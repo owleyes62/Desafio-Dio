2a2565694ebbb06a231ddb672b825435f3fc24fd
Iwerson <guilhermepdo2012@gmail.com>
2023-12-20 21:58:52 -0300

    Terceiro desafio Dio Escrevendo as Classes de um jogo

---
+++ EscrevendoAsClassesDeUmJogo.py
@@ -0,0 +1,19 @@
+class Heroi():
+    nome = 'cacau'
+    idade = 23 
+    tipo = 'guerreiro'
+
+    def Ataque (self):
+        if self.tipo == 'guerreiro':
+            return  f"o guerreio {self.nome} atacou usando espada"
+        if self.tipo == 'mago':
+            return f"o mago {self.nome} atacou usando magia"
+        if self.tipo == 'monge':
+            return f"o monge {self.nome} atacou usando artes marciais"
+        if self.tipo == 'ninja':
+            return f"o ninja {self.nome} atacou usando shuriken"
+     
+
+heroi_cacau = Heroi()
+resultado_ataque = heroi_cacau.Ataque()
+print(resultado_ataque)
